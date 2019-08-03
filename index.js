6c37cfc235af6b7f3a41bda80881e8b879c557c2
Gautham Yerroju <gautham.yerroju@gmail.com>
2019-08-03 16:55:21 -0700

    add more cmdline args for tests

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ const { iterDirItems } = require('./tools.js')
 const media = require('./media')
 
 
-const recurse = process.argv[2] || config.get('fs.recurse')
-const dir = process.argv[3] || config.get('fs.defaultDir')
-console.log(dir, recurse)
+const dir = process.argv[2] || config.get('fs.defaultDir')
+const recurse = process.argv[3] || config.get('fs.recurse')
+const sortBy = process.argv[4] || 'name'
+console.log(dir, recurse, sortBy)
 
 
-for (const item of iterDirItems(dir, recurse)) {
+for (const item of iterDirItems(dir, recurse, sortBy)) {
     media.inspect(item.path).then(meta => console.log(meta))
 }
